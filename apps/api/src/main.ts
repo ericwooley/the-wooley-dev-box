3fd5c7fc550c4cbf31cd7e672177d2c765183e22
Eric Wooley <ericwooley@gmail.com>
2021-03-27 11:56:46 -0700

    add apollo server express

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,18 +1,29 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import * as express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
+(async () => {
+  const app = express();
+  app.get('/api', (req, res) => {
+    res.send({ message: 'Welcome to api!' });
+  });
+  const port = process.env.port || 3333;
+  // Construct a schema, using GraphQL schema language
+  const typeDefs = gql`
+    type Query {
+      hello: String
+    }
+  `;
+  // Provide resolver functions for your schema fields
+  const resolvers = {
+    Query: {
+      hello: () => 'Hello world!',
+    },
+  };
+  const server = new ApolloServer({ typeDefs, resolvers });
+  await server.start();
+  server.applyMiddleware({ app, path: '/graphql' });
 
-const app = express();
-
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to api!' });
-});
-
-const port = process.env.port || 3333;
-const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
-});
-server.on('error', console.error);
+  await new Promise<void>((resolve) => app.listen({ port }, resolve));
+  console.log(
+    `🚀 Server ready at http://localhost:${port}${server.graphqlPath}`
+  );
+})();
