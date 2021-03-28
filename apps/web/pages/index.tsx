2733a09eb5f94221312ba53a97df69e46adc9c99
Eric Wooley <ericwooley@gmail.com>
2021-03-28 15:09:03 -0700

    initialize-apollo-client

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 
 import styles from './index.module.css';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+const client = new ApolloClient({
+  uri: 'http://localhost:3333/graphql',
+  cache: new InMemoryCache(),
+});
 
 export function Index() {
   return (
-    <div className={styles.page}>
-      <h3>My Lists (25):</h3>
-    </div>
+    <ApolloProvider client={client}>
+      <div className={styles.page}>
+        <h3>My Lists (25):</h3>
+      </div>
+    </ApolloProvider>
   );
 }
 
