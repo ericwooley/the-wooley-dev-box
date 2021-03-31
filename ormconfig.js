8e7ec85113553ab89c08126582e77a8c945e55f0
Eric Wooley <ericwooley@gmail.com>
2021-03-31 08:38:54 -0700

    add official database url

--- ormconfig.js
+++ ormconfig.js
@@ -1,15 +1,22 @@
+const envDependentVars = process.env.DATABASE_URL
+  ? {
+      url: process.env.DATABASE_URL,
+    }
+  : {
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      host: 'localhost',
+      port: 5432,
+    };
 module.exports = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
   synchronize: false,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'postgres',
   entities: ['libs/db/src/lib/entity/**/*.ts'],
   migrations: ['libs/db/src/lib/migration/**/*.ts'],
   subscribers: ['libs/db/src/lib/subscriber/**/*.ts'],
   cli: {
     migrationsDir: 'libs/db/src/lib/migration',
   },
+  ...envDependentVars,
 };
