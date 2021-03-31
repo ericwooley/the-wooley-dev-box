09e18525b18cec9ff85a16ea6a10c5d1c6d30150
Eric Wooley <ericwooley@gmail.com>
2021-03-30 17:39:38 -0700

    WIP

--- scripts/deploy-api.sh
+++ scripts/deploy-api.sh
@@ -1,4 +1,13 @@
+echo "Login to container"
+heroku container:login || exit 1
+
+echo "Push migration"
+heroku container:push release -a thewooley-devbox || exit 1
+echo "Release migration"
+heroku container:release release -a thewooley-devbox || exit 1
+
 cd dist/apps/api
-heroku container:login
+echo "Push api"
 heroku container:push web -a the-wooley-devbox
+echo "Release api"
 heroku container:release web -a the-wooley-devbox
