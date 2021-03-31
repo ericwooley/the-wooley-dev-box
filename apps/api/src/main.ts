f3a652c0f32f4eea9736c856808956f81a849250
Eric Wooley <ericwooley@gmail.com>
2021-03-31 09:05:14 -0700

    create connection in app

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,17 +6,27 @@ import { resolvers } from '@the-wooley-devbox/graphql';
 import { buildSchema } from 'type-graphql';
 import { createConnection } from 'typeorm';
 import { List, TodoItem } from '@the-wooley-devbox/db';
+const envDependentVars = process.env.DATABASE_URL
+  ? {
+      url: `${process.env.DATABASE_URL}`,
+      // https://devcenter.heroku.com/articles/heroku-postgresql#connecting-in-node-js
+      ssl: { rejectUnauthorized: false },
+    }
+  : {
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      host: 'localhost',
+      port: 5432,
+    };
+
 (async () => {
   // connect to database, before we bootstrap the app.
   await createConnection({
-    logging: process.env.NODE_ENV === 'development',
     type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'postgres',
-    database: 'postgres',
+    synchronize: false,
     entities: [List, TodoItem],
+    ...envDependentVars,
   });
   const app = express();
   setInterval(() => {
