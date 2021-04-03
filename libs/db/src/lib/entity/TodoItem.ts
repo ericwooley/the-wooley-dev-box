2d1695c8538df68e04f78c86475029ee1962943b
Eric Wooley <ericwooley@gmail.com>
2021-04-03 10:53:15 -0700

    update screenshots

--- libs/db/src/lib/entity/TodoItem.ts
+++ libs/db/src/lib/entity/TodoItem.ts
@@ -1,23 +1,13 @@
-import { Field, ID, ObjectType } from 'type-graphql';
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
-import { List } from './List';
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
 @Entity()
-@ObjectType()
 export class TodoItem {
-  @Field(() => ID)
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Field()
   @Column()
   name: string;
 
-  @Field()
   @Column({ default: false })
   done: boolean;
-
-  @Field(() => List)
-  @ManyToOne(() => List, (list) => list.items)
-  list: Promise<List>;
 }
